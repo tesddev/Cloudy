98bddf14815b65f12ee3c0afce98cf4f1c21db98
tesddev <tesleem.amuda@gmail.com>
2023-01-03 11:00:33 +0100

    add computed property date to view model

--- Cloudy/View Controllers/Weather View Controllers/View Models/DayViewModel.swift
+++ Cloudy/View Controllers/Weather View Controllers/View Models/DayViewModel.swift
@@ -2,8 +2,18 @@
 //  DayViewModel.swift
 //  Cloudy
 //
-//  Created by GIGL iOS on 02/01/2023.
+//  Created by Tes on 02/01/2023.
 //  Copyright © 2023 Cocoacasts. All rights reserved.
 //
 
 import Foundation
+
+struct DayViewModel {
+    let weatherData: WeatherData
+    
+    var date: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE, MMMM d"
+        return dateFormatter.string(from: weatherData.time)
+    }
+}
